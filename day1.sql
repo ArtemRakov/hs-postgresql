7f1c920b42bbbfb6b4f004b11e441d589141d7a2
Artem Rakov <scoff1k@gmail.com>
2021-04-13 20:49:24 -0400

    day1

--- day1.sql
+++ day1.sql
@@ -1,5 +1,21 @@
-SELECT gaps.id + 1 as start
+SELECT gaps.id + 1 as from
+       MIN(fr.id) - 1 as to
 FROM gaps
 LEFT JOIN gaps r on gaps.id = r.id - 1
-WHERE r.id is null;
+LEFT JOIN gaps fr on gaps.id < fr.id
+where r.id is null
+      and fr.id is not null
+group by gaps.id,
+         r.id;
 
+with gg as (
+     select id, lag(id) over () mn, lead(id) over () mx from gaps
+ )
+ select id + 1, mx - 1
+ from gg
+ where id + 1 < mx;
+
+select prev_id + 1 as "from", id - 1 as "to"  from (
+    select gg.id, lag(id) over (order by id) as prev_id from gaps gg
+  ) tt
+where id - prev_id > 1;
